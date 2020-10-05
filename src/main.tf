da3c9a86b1b4e5e81a20dfa52e4a40cfae749def
Faisal Kamal <faisal.fareed@live.com>
2020-10-05 00:36:07 +0000

    updates to mai.tf

--- src/main.tf
+++ src/main.tf
@@ -1,10 +1,34 @@
 provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "=2.20.0"
   features {}
 }
 
-resource "azurerm_resource_group" "az-tf-demo" {
-  name     = "az-tf-demo"
-  location = "Australia SouthEast"
+resource "azurerm_resource_group" "demo" {
+  name     = "${var.prefix}-resources"
+  location = "${var.location}"
 }
+
+resource "azurerm_storage_account" "demo" {
+  name                     = "637374939350000000"
+  resource_group_name      = azurerm_resource_group.demo.name
+  location                 = azurerm_resource_group.demo.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  network_rules {
+    default_action = "Deny"
+    ip_rules       = ["23.45.1.0/30"]
+  }
+}
+
+resource "azurerm_app_service_plan" "demo" {
+  name                = "${var.prefix}-asp"
+  resource_group_name = azurerm_resource_group.demo.name
+  location            = azurerm_resource_group.demo.location
+  kind                = "FunctionApp"
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+} 
