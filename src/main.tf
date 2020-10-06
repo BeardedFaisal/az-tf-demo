b2f3cf079b882c5e762012659d254e699c0e7906
Faisal Kamal <faisal.fareed@live.com>
2020-10-06 00:49:09 +0000

    Add AppServicePlan and AppService to main.tf

--- src/main.tf
+++ src/main.tf
@@ -41,6 +41,40 @@ resource "azurerm_storage_account" "demo" {
   }
 }
 
+resource "azurerm_app_service_plan" "demo" {
+  name                = "demo-appserviceplan"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "demo" {
+  name                = "demo-app-service"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+  app_service_plan_id = azurerm_app_service_plan.demo.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
+}
+
+
+
 # App Service Plan
 /*resource "azurerm_app_service_plan" "demo" {
   name                = "${var.prefix}-asp"
